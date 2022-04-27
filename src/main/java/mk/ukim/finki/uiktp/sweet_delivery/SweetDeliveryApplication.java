a0da1c59fffe545ca40830e38b533559b24419af
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2022-04-27 20:37:41 +0200

    Add Project Files

---
+++ src/main/java/mk/ukim/finki/uiktp/sweet_delivery/SweetDeliveryApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uiktp.sweet_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetDeliveryApplication.class, args);
+	}
+
+}
