2c3a07154070788e1af6b33924dce9f0e9d0e832
dangev9 <martindangev99@gmail.com>
2022-05-18 09:53:11 +0200

    Post controller created

--- src/main/java/mk/ukim/finki/uiktp/sweet_delivery/service/PostService.java
+++ src/main/java/mk/ukim/finki/uiktp/sweet_delivery/service/PostService.java
@@ -2,11 +2,12 @@ package mk.ukim.finki.uiktp.sweet_delivery.service;
 
 import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.Post;
 import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.Recipe;
+import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.dto.PostDTO;
 import mk.ukim.finki.uiktp.sweet_delivery.model.userroles.User;
 
 import java.util.Optional;
 
 public interface PostService {
-    Optional<Post> createPost(Recipe recipe, User user);
-    Optional<Post> deletePost(Long postId);
+    Post createPost(PostDTO postDTO);
+    void deletePost(Long postId);
 }
--- src/main/java/mk/ukim/finki/uiktp/sweet_delivery/service/impl/PostServiceImpl.java
+++ src/main/java/mk/ukim/finki/uiktp/sweet_delivery/service/impl/PostServiceImpl.java
@@ -1,10 +1,14 @@
 package mk.ukim.finki.uiktp.sweet_delivery.service.impl;
 
 import mk.ukim.finki.uiktp.sweet_delivery.model.exceptions.PostNotFoundException;
+import mk.ukim.finki.uiktp.sweet_delivery.model.exceptions.UserNotFoundException;
 import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.Post;
 import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.Recipe;
+import mk.ukim.finki.uiktp.sweet_delivery.model.metamodel.dto.PostDTO;
 import mk.ukim.finki.uiktp.sweet_delivery.model.userroles.User;
 import mk.ukim.finki.uiktp.sweet_delivery.repository.PostRepository;
+import mk.ukim.finki.uiktp.sweet_delivery.repository.RecipeRepository;
+import mk.ukim.finki.uiktp.sweet_delivery.repository.UserRepository;
 import mk.ukim.finki.uiktp.sweet_delivery.service.PostService;
 import org.springframework.stereotype.Service;
 
@@ -15,26 +19,33 @@ import java.util.Optional;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final RecipeRepository recipeRepository;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, UserRepository userRepository, RecipeRepository recipeRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     @Override
-    public Optional<Post> createPost(Recipe recipe, User user) {
+    public Post createPost(PostDTO postDTO) {
 
-        if(recipe!=null && user!=null) {
+        Recipe recipe = this.recipeRepository.getById(postDTO.getRecipeId());
+        User user = this.userRepository.findById(postDTO.getUserId()).orElseThrow(UserNotFoundException::new);
+        Post post = new Post();
+        if(user != null) {
             OffsetDateTime date_created = OffsetDateTime.now();
-            return Optional.of(this.postRepository.save(new Post(recipe, date_created, user)));
+            post.setDate_created(date_created);
+            post.setRecipe(recipe);
+            post.setUser(user);
         }
-
-        return Optional.empty();
+        return post;
     }
 
     @Override
-    public Optional<Post> deletePost(Long postId) {
+    public void deletePost(Long postId) {
         Post post = this.postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
         this.postRepository.deleteById(postId);
-        return Optional.of(post);
     }
 }
